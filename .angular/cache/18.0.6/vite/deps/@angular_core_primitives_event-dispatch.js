4288fc623904ac9adbe6d232654eadee673134cf
Ljuelch <leojuelch@yahoo.de>
2024-06-28 19:07:22 +0200

    new: tailwindcss setup

---
+++ .angular/cache/18.0.6/vite/deps/@angular_core_primitives_event-dispatch.js
@@ -0,0 +1,38 @@
+import {
+  Attribute,
+  EventContract,
+  EventContractContainer,
+  EventDispatcher,
+  EventInfoWrapper,
+  EventPhase,
+  JSACTION$1,
+  JSINSTANCE,
+  JSTRACK,
+  OI$1,
+  VED,
+  VET,
+  bootstrapEarlyEventContract,
+  isCaptureEvent,
+  isSupportedEvent,
+  registerDispatcher
+} from "./chunk-JKBTTGCY.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  Attribute,
+  EventContract,
+  EventContractContainer,
+  EventDispatcher,
+  EventInfoWrapper,
+  EventPhase,
+  JSACTION$1 as JSACTION,
+  JSINSTANCE,
+  JSTRACK,
+  OI$1 as OI,
+  VED,
+  VET,
+  bootstrapEarlyEventContract,
+  isCaptureEvent,
+  isSupportedEvent,
+  registerDispatcher
+};
+//# sourceMappingURL=@angular_core_primitives_event-dispatch.js.map
--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
-import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
@@ -35,7 +34,6 @@ export function app(): express.Express {
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
