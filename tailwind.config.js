df116ccb681fa074e42add553dc2c6ab8bbfc46d
ecleng <ecleng@nettrek.de>
2024-06-30 23:34:48 +0200

    add pwa, add material, configure angularjson,add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,24 @@ module.exports = {
   content: [ // purge id deprecated but treeshaking still works
     "./src/**/*.{html,ts}",
   ],
-  theme: {
-    extend: {},
+  theme: { // custom overwritten properties
+    screens: {
+      sm: '500px',
+      md: '800px',
+      lg: '1200px',
+      xl: '1500px',
+    },
+    colors: {
+      'primary': '#09f',
+      'secondary': '#00ae00',
+      'text': '#323232',
+      'black': '#000',
+      'white': '#fff',
+      'error': '#f00',
+    },
+    fontFamily: {
+      sans: ['Asap', 'sans-serif'] // sans class => global default family
+    },
   },
   plugins: [],
 }
